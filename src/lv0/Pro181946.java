aa390b080f5039aee3fbefc3a102c72d08ce56dc
hjhjhj0917 <abc22842199@gmail.com>
2025-06-10 09:48:49 +0900

    25.05.26

---
+++ src/lv0/Pro181946.java
@@ -0,0 +1,14 @@
+package lv0;
+
+import java.util.Scanner;
+
+public class Pro181946 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a + b);
+    }
+}
